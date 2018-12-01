2b6a59a62edfc2163a445f5f14a62ebcc74cf5bc
slayershow <slayershow@users.noreply.github.com>
2018-12-01 16:31:27 -0200

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,29 +1,30 @@
-var x = 75; var y = 0;//posiÁ„o do gatinho
-var oX = 975;var oY = 75;//posiÁ„o da maosinha inicial
-var oY1 = 75;var oY2 = 75;var oY3 = 75;//posiÁ„o Y das maosinhas adicionais, como a posiÁ„o X
-//o X È igual pra todas
+var x = 75; var y = 0;//posi√ß√£o do gatinho
+var oX = 975;var oY = 75;//posi√ß√£o da maosinha inicial
+var oY1 = 75;var oY2 = 75;var oY3 = 75;//posi√ß√£o Y das maosinhas adicionais, como a posi√ß√£o X
+//o X √© igual pra todas
 var oX1; var oX2;var cont = 0; var l = 0;
-var gato = []; var mao = [];
+var gato = []; var tronco = [];
 var condicao = 1;
-var ambiente;
+var ambiente = []; var indice = [];
 var tela = 1;var regressao = 10;
-	var cont2 = 0;
+	var cont2 = 0; var cont3=0;
 
-var pontuacao = 0;//contador de pontuaÁ„o
+var pontuacao = 0;//contador de pontua√ß√£o
 
 function preload() { 
 	for(i=0;i<4;i++){
-		mao[i] = loadImage("figuras/mao"+i+".png");
+		tronco[i] = loadImage("figuras/tronco"+i+".png");
 	}
 	for(i=0;i<4;i++){
 		gato[i] = loadImage("figuras/gato"+i+".png");
 	}
-
-	ambiente = loadImage("figuras/ambiente.jpg");
+	for(i=0;i<10;i++){
+		ambiente[i] = loadImage("figuras/ambiente"+i+".png");
+	}
 }
 
 function setup() {
- 	createCanvas(1001, 600);
+ 	createCanvas(1000, 563);
 }
 
 function draw(){
@@ -31,6 +32,9 @@ function draw(){
 	background(0);
 
 	if(tela == 1){
+		for(i=0;i<10;i++){
+			indice[i] = i;
+		}
 		textSize(28); 
 		fill(200);
 		text("Pressione ENTER para iniciar", 50, 50);
@@ -42,24 +46,36 @@ function draw(){
 
 
 	if(tela == 2){
-	//CONTAGEM DA PONTUA«√O
+
+
+	//CONTAGEM DA PONTUA√á√ÉO
 	if(pontuacao < 1000){
 		pontuacao++;
 	}
 
 			
-	image(ambiente,0,0);
-	//CONDI«√O PARA MUDAN«A DE NÕVEL
+	image(ambiente[indice[0]],0,0);
+	image(ambiente[indice[1]],100,0);
+	image(ambiente[indice[2]],200,0);
+	image(ambiente[indice[3]],300,0);
+	image(ambiente[indice[4]],400,0);
+	image(ambiente[indice[5]],500,0);
+	image(ambiente[indice[6]],600,0);
+	image(ambiente[indice[7]],700,0);
+	image(ambiente[indice[8]],800,0);
+	image(ambiente[indice[9]],900,0);
+
+	//CONDI√á√ÉO PARA MUDAN√áA DE N√çVEL
 	//NIVEL 2
 	if(pontuacao >= 1000 && pontuacao < 3000){//ADICIONA OUTRA MAOSINHA
 
 		pontuacao+=2;//AUMENTANDO A PONTUACAO PELA DIFICULDADE
 
-		image(mao[l],oX1,oY1);//SEGUNDA MAOSINHA
+		image(tronco[l],oX1,oY1);//SEGUNDA MAOSINHA
 		
 		
 		
-		//DETECTANDO COLIS√O COM A SEGUNDA MAOSINHA
+		//DETECTANDO COLIS√ÉO COM A SEGUNDA MAOSINHA
 		if(oX1 <= 115){
 			var condicao = y-oY1;
 			if(condicao < 50 && condicao > -50){
@@ -73,9 +89,9 @@ function draw(){
 	if(pontuacao >= 3000){//ADICIONA OUTRA MAOSINHA
 		pontuacao+=5;//AUMENTANDO A PONTUACAO
 
-		image(mao[l],oX1,oY1);//SEGUNDA MAOSINHA
+		image(tronco[l],oX1,oY1);//SEGUNDA MAOSINHA
 		
-		//DETECTANDO COLIS√O COM A SEGUNDA MAOSINHA
+		//DETECTANDO COLIS√ÉO COM A SEGUNDA MAOSINHA
 		if(oX1 <= 115){
 		var condicao = y-oY1;
 			if(condicao < 50 && condicao > -50){
@@ -83,9 +99,9 @@ function draw(){
 			}
 		}
 
-		image(mao[l],oX2,oY2);
+		image(tronco[l],oX2,oY2);
 		
-		//DETECTANDO COLIS√O COM A TERCEIRA MAOSINHA
+		//DETECTANDO COLIS√ÉO COM A TERCEIRA MAOSINHA
 		if(oX2 <= 115){
 			var condicao = y-oY2;
 			if(condicao < 50 && condicao > -50){
@@ -96,7 +112,7 @@ function draw(){
 	}
 	
 	
-	//DETECTANDO COLIS√O COM A PRIMEIRA MAOSINHA
+	//DETECTANDO COLIS√ÉO COM A PRIMEIRA MAOSINHA
 	if(oX <= 115){
 		var condicao = y-oY;
 		if(condicao < 50 && condicao > -50){
@@ -104,7 +120,7 @@ function draw(){
 		}
 	}
 			
-	//CAPTURA DE TECLADO E MOVIMENTA«√O VERTICAL DO GATINHO
+	//CAPTURA DE TECLADO E MOVIMENTA√á√ÉO VERTICAL DO GATINHO
 	if(keyIsDown(UP_ARROW)){
 		if(y >= 5)
 				y -= 5;
@@ -118,12 +134,12 @@ function draw(){
 	
 	
 	
-	image(mao[l],oX,oY);//REPRESENTA«√O GR¡FICA DA PRIMEIRA MAOSINHA
+	image(tronco[l],oX,oY);//REPRESENTA√á√ÉO GR√ÅFICA DA PRIMEIRA MAOSINHA
 
-	image(gato[l],x,y);//REPRESENTA«√O DO GATINHO
+	image(gato[l],x,y);//REPRESENTA√á√ÉO DO GATINHO
 	
 	//GERANDO VALORES DO EIXO X DAS MAOSINHAS, QUANDO ATRAVESSA O 
-	//CEN¡RIO, RESETA OS VALORES.
+	//CEN√ÅRIO, RESETA OS VALORES.
 	if(oX > 25){
 		oX -= 5;
 		if(pontuacao >= 1000 && pontuacao < 3000){
@@ -165,8 +181,15 @@ function draw(){
 		oX2 = 975;
 		oY2 = random(475);
 	}
-
-
+	cont2++;
+	if(cont2 == 5){
+		var aux = indice [0];
+		for(i=0;i<9;i++){
+			indice[i] = indice[i+1]
+		}
+		indice[9] = aux;
+		cont2 = 0
+	}
 
 
 
@@ -188,7 +211,8 @@ function draw(){
 		l = 0;
 	}
 
-	//TEXTO DA PONTUA«√O
+
+	//TEXTO DA PONTUA√á√ÉO
 	textSize(50);
 	fill(50);
 	text("Pontos: " + pontuacao + " ", 600, 65);
@@ -238,7 +262,10 @@ function draw(){
 
 		if(regressao == 0){
 			tela = 1;
-			regressao = 0;
+			regressao = 10;
+			cont2=0;
+			pontuacao=0;
+			oX = 975;oX1 = 975;oX2 = 975;
 		}
 	}
 }
